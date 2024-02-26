b3d25acb2868db5cdb70c1e2c9028f2bb9730333
Daniel Sierra Moreno <daniel.sierra@enti.cat>
2024-02-26 09:22:02 +0100

    clase 26/2

--- Redes2_FirstProject/Redes2_FirstProject/SocketSelect.cpp
+++ Redes2_FirstProject/Redes2_FirstProject/SocketSelect.cpp
@@ -1,1 +1,36 @@
 #include "SocketSelect.h"
+
+bool SocketSelect::Wait()
+{
+	_mutex.lock();
+	bool ready = wait();
+	_mutex.unlock();
+
+	return false;
+}
+
+bool SocketSelect::IsReady(TcpListener* listener)
+{
+	return false;
+}
+
+bool SocketSelect::IsReady(TcpSocket& socket)
+{
+	return false;
+}
+
+void SocketSelect::Add(TcpListener* listener)
+{
+}
+
+void SocketSelect::Add(TcpSocket& socket)
+{
+}
+
+void SocketSelect::Remove(TcpListener* listener)
+{
+}
+
+void SocketSelect::Remove(TcpSocket& socket)
+{
+}
--- Redes2_FirstProject/Redes2_FirstProject/SocketsManager.cpp
+++ Redes2_FirstProject/Redes2_FirstProject/SocketsManager.cpp
@@ -1,1 +1,83 @@
 #include "SocketsManager.h"
+
+SocketsManager::SocketsManager(OnSocketConnected onSocketConnected)
+{
+}
+
+SocketsManager::~SocketsManager()
+{
+	delete _listener;
+	for (TcpSocket* socket : _sockets)
+	{
+		delete socket;
+	}
+}
+
+void SocketsManager::StartLoop()
+{
+	_isRunningMutex.lock();
+
+	if (_isRunning)
+	{
+		_isRunningMutex.unlock();
+		return;
+	}
+
+	_isRunning = true;
+	_isRunningMutex.unlock();
+
+	std::thread* loopThread = new std::thread(&SocketsManager::SelectorLoop, this);
+	loopThread->detach();
+}
+
+bool SocketsManager::StartListener(unsigned short port)
+{
+	return false;
+}
+
+bool SocketsManager::ConnectToServer(std::string ip, unsigned short port)
+{
+	return false;
+}
+
+void SocketsManager::SelectorLoop()
+{
+	_isRunningMutex.lock();
+	bool isRunning = true;
+	_isRunningMutex.unlock();
+
+
+	while (isRunning)
+	{
+		if (_selector.Wait())
+		{
+			//TODO
+		}
+
+		_isRunningMutex.lock();
+		 isRunning = false;
+		_isRunningMutex.unlock();
+
+	}
+
+}
+
+void SocketsManager::CheckListener()
+{
+}
+
+void SocketsManager::CheckSockets()
+{
+}
+
+void SocketsManager::AddSocket(TcpSocket* socket)
+{
+}
+
+void SocketsManager::RemoveSocket(TcpSocket* socket)
+{
+}
+
+void SocketsManager::RemoveSocketAsync(TcpSocket* socket)
+{
+}
